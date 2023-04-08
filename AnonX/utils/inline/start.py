af899098f646a62bd448757f95f3424cc9188c6e
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-08 10:01:16 +0300

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -173,6 +173,7 @@ REPLY_MESSAGE_BUTTONS = [
 ]
 
   
+    
 @app.on_message(filters.private & filters.command("commands"))
 async def cpanel(_, message: Message):             
         text = REPLY_MESSAGE
